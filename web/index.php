bc369ef5d4d2ec9510c9c055a88922d223466966
Mazdak Mohammadi <mazdakmohammadi@Mazdaks-MacBook-Pro.local>
2025-07-15 07:47:23 -0700

    wip

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ $pass = $_SERVER['PHP_AUTH_PW'];
 $validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
 
 if (!$validated) {
-  header('WWW-Authenticate: Basic realm="My Realm"');
+  header('WWW-Authenticate: Basic');
   header('HTTP/1.0 401 Unauthorized');
   die ("Not authorized");
 }
